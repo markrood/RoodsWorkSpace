f585fb02fd9c76cb9c6a94c35905eb19d32c0fd9
markrood <markdamonroof@gmail.com>
2023-12-28 13:12:02 -0600

    original

---
+++ Esp32_AToAwc/lib/ATO/ato.cpp
@@ -0,0 +1,12 @@
+#include "ato.h"
+
+Ato::Ato(){
+    
+}
+
+bool Ato::doAto(){
+    bool ret = false;
+    Serial.println("in doAto");
+    ret = true;
+    return ret;
+}
---
+++ Esp32_AToAwc/lib/AWC/awc.cpp
@@ -0,0 +1,12 @@
+#include "awc.h"
+
+Awc::Awc(){
+    //code here
+}
+
+bool Awc::doAwc(){
+    bool ret = false;
+    Serial.println("in doAwc");
+    ret = true;
+    return ret;
+}
